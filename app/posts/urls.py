a189f20a6395c3173ded4f843eea3b5d4ae0d8d7
tanitaDev <tanita.pak@gmail.com>
2022-11-28 17:03:30 +0600

    Этап лайки 1. Добавлен функционал лайков

--- app/posts/urls.py
+++ app/posts/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
 from posts.views.base import IndexView
+from posts.views.post_like import LikePost
 from posts.views.search import SearchView
 from posts.views.posts import AddPost
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('add-post', AddPost.as_view(), name='add_post'),
-    path('search', SearchView.as_view(), name='search')
+    path('search', SearchView.as_view(), name='search'),
+    path('like/<int:pk>', LikePost.as_view(), name='like')
 ]
